9acc980221da060f9558df31f11d96e4ef7c95aa
priyankahotkar <160253899+priyankahotkar@users.noreply.github.com>
2025-05-08 11:22:07 +0530

    Initial commit3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-2.0/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
